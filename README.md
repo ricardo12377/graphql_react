8a950c66750edaad0c3c6f6b9398faa2bf156cb6
ricardo <rcostaj00@gmail.com>
2022-05-25 22:03:30 -0300

    first

---
+++ README.md
@@ -0,0 +1,5 @@
+yarn add @apollo/client graphql
+
+1 - provider in index.js.
+
+2 - in context import  gql e  use query
---
+++ src/routes/category/category.component.jsx
@@ -0,0 +1,36 @@
+import { useContext, useState, useEffect, Fragment } from 'react';
+import { useParams } from 'react-router-dom';
+
+import ProductCard from '../../components/product-card/product-card.component';
+import Spinner from '../../components/spinner/spinner.component';
+
+import { CategoriesContext } from '../../contexts/categories.context';
+
+import { CategoryContainer, Title } from './category.styles';
+
+const Category = () => {
+  const { category } = useParams();
+  const { categoriesMap, loading } = useContext(CategoriesContext);
+  const [products, setProducts] = useState(categoriesMap[category]);
+
+  useEffect(() => {
+    setProducts(categoriesMap[category]);
+  }, [category, categoriesMap]);
+
+  return (
+    <Fragment>
+     {loading ? <Spinner /> : (
+       <Fragment> 
+       <Title>{category.toUpperCase()}</Title>
+      <CategoryContainer>
+        {products &&
+          products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+      </CategoryContainer>
+      </Fragment>)}
+    </Fragment>
+  );
+};
+
+export default Category;
